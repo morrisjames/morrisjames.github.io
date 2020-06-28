3b4bd6353c7ebc79500dd5ea808b81b81e268bbb
piyush.nanwani <pnanwani61@gmail.com>
2020-06-28 16:08:48 +0530

    added slideshow to products

---
+++ js/products.js
@@ -0,0 +1,31 @@
+setTimeout(function() {
+    document.getElementById("hospital-products").setAttribute("src", "img/products/beds/agora.png");
+    }, 3000);
+    
+setTimeout(function() {
+    document.getElementById("medical-devices").setAttribute("src", "img/products/medical-devices/uv-sanitizer-box.jpg");
+    }, 6000);
+
+setTimeout(function() {
+    document.getElementById("consumables-accessories").setAttribute("src", "img/products/consumables-accessories/sanitizer.jpg");
+    }, 9000);
+
+setTimeout(function() {
+    document.getElementById("others").setAttribute("src", "img/products/accessories/item2.png");
+    }, 12000);
+
+setTimeout(function() {
+    document.getElementById("hospital-products").setAttribute("src", "img/products/furniture/item4.png");
+    }, 15000);
+    
+setTimeout(function() {
+    document.getElementById("medical-devices").setAttribute("src", "img/products/medical-devices/thermometer.jpg");
+    }, 18000);
+
+setTimeout(function() {
+    document.getElementById("consumables-accessories").setAttribute("src", "img/products/consumables-accessories/mask.jpg");
+    }, 21000);
+
+setTimeout(function() {
+    document.getElementById("others").setAttribute("src", "img/products/accessories/item3.png");
+    }, 24000);
